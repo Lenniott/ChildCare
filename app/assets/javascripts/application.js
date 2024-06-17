0d2299e215f1bde392d897c25b4dec77950dbb54
Ben Kanagroo <78645337+Lenniott@users.noreply.github.com>
2024-06-17 22:57:57 +0100

    made it more realistic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,19 +12,26 @@ window.GOVUKPrototypeKit.documentReady(() => {
       const nursaryOneInvoiceAmountClaire = document.getElementById('invoice-amount-1-ct').value;
       const nursaryTwoInvoiceAmountEmma = document.getElementById('invoice-amount-2-et').value;
       const nursaryTwoInvoiceAmountClaire = document.getElementById('invoice-amount-2-ct').value;
+      const childOneBalance = "£65.40"
+      const childTwoBalance = "£63.10"
 
-      const emmaTotal = parseFloat(nursaryOneInvoiceAmountEmma) + parseFloat(nursaryTwoInvoiceAmountEmma);
+      const childOneContribution = parseFloat(childOneBalance.replace("£", "")) * 0.8;
+      const childTwoContribution = parseFloat(childTwoBalance.replace("£", "")) * 0.8;
+
+
+
+      const emmaTotal = parseFloat(nursaryOneInvoiceAmountEmma) + parseFloat(nursaryTwoInvoiceAmountEmma) ;
       const claireTotal = parseFloat(nursaryOneInvoiceAmountClaire) + parseFloat(nursaryTwoInvoiceAmountClaire);
 
-      const emmaTransfer = emmaTotal*0.8;
-      const claireTransfer = claireTotal*0.8;
+      const emmaTransfer = (emmaTotal - childOneContribution)*0.8;
+      const claireTransfer = (claireTotal - childTwoContribution)*0.8;
       
       // Update the inner text of each span with the corresponding invoice amount
-      document.querySelector('#result-et span').textContent = `£ ${emmaTransfer}`;
-      document.querySelector('#result-2-et span').textContent = `£ ${claireTransfer*1.25}`;
+      document.querySelector('#result-et span').textContent = `£ ${parseFloat(emmaTransfer.toFixed(2))}`;
+      document.querySelector('#result-2-et span').textContent = `£ ${parseFloat(((claireTransfer+childOneContribution)*1.25).toFixed(2))}`;
 
-      document.querySelector('#result-ct span').textContent = `£ ${claireTransfer}`;
-      document.querySelector('#result-2-ct span').textContent = `£ ${claireTransfer*1.25}`;
+      document.querySelector('#result-ct span').textContent = `£ ${parseFloat(claireTransfer.toFixed(2))}`;
+      document.querySelector('#result-2-ct span').textContent = `£ ${parseFloat(((claireTransfer+childTwoContribution)*1.25).toFixed(2))}`;
 
 
       // Make the results div visible
